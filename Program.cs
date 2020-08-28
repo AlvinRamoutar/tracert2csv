4787d16045d6536ba1ab6fd993c1ed295ae614da
Alvin <alvinramoutar@gmail.com>
2020-08-27 22:14:04 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace tracert2csv {
+    class Program {
+        static void Main(string[] args) {
+
+            bool started = false;
+            string line;
+            List<string> lines = new List<string>();
+            List<Traceroute> traces = new List<Traceroute>();
+
+            StreamReader file = new StreamReader(@"tracerts\test.txt");
+            Regex regex = new Regex("^  1.*");
+
+            while ((line = file.ReadLine()) != null) {
+
+                if (line.Length == 0 && started) {
+                    break;
+                }
+                else if (!started) {
+                    if(regex.IsMatch(line)) {
+                        started = true;
+                        lines.Add(line);
+                    }
+                }
+                else {
+                    lines.Add(line);
+                }
+            }
+
+            foreach(string l in lines) {
+                Traceroute tr = new Traceroute(l);
+                traces.Add(tr);
+            }
+
+            WriteOutput(traces, args[1]);
+
+        }
+
+        static void WriteOutput(ICollection<Traceroute> traces, string path) {
+
+            using (StreamWriter swriter = new StreamWriter(path)) {
+
+                foreach(Traceroute tr in traces) {
+                    swriter.WriteLine(tr.ToCommaDelimitedString());
+                }
+                swriter.Flush();
+            }
+        }
+    }
+}
---
+++ Traceroute.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace tracert2csv {
+    struct Traceroute {
+
+        public int Index { get; private set; }
+
+        public ICollection<int> TransitDelays { get; private set; }
+
+        public double AverageDelay { get; private set; }
+
+        public string Host { get; private set; }
+
+        public bool IsTimedOut { get; private set; }
+
+        public Traceroute(string line) {
+
+            char[] l = line.ToCharArray();
+            l[6] = ',';
+            l[15] = ',';
+            l[24] = ',';
+            l[31] = ',';
+
+            string[] pieces = new string(l).Split(',');
+
+            Index = int.Parse(pieces[0].Trim());
+
+            TransitDelays = new List<int>();
+            AverageDelay = 0;
+            for(int i = 1; i <= pieces.Length - 2; i++) {
+                string p = pieces[i].Trim();
+                int d = 0;
+
+                if(!p.Contains("*")) {
+                    d = int.Parse(Regex.Match(p, @"\d+").Value);
+                }
+
+                TransitDelays.Add(d);
+
+                AverageDelay += d;
+            }
+            AverageDelay = AverageDelay / (pieces.Length - 2);
+
+            Host = pieces[pieces.Length - 1].Trim();
+
+            IsTimedOut = (line.Split('*').Length - 1) == 3;
+
+        }
+
+        public string ToCommaDelimitedString() {
+
+            string compiled = Index.ToString() + ",";
+
+            foreach(int td in TransitDelays) {
+                compiled += td.ToString() + ",";
+            }
+
+            compiled += Host;
+            return compiled;
+        }
+
+
+    }
+}
